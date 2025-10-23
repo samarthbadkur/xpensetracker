fe0decb8166b2496edea03ab89ea4f0f1db288ba
Samarth Badkur <samarthbadkur11@gmail.com>
2025-10-23 13:55:13 +0530

    modified

--- src/Components/WalletModal.jsx
+++ src/Components/WalletModal.jsx
@@ -1,11 +1,10 @@
 import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
 import { Grid } from "@mui/material";
-import { useState } from "react";
 
 const style = {
   position: "absolute",
@@ -19,17 +18,10 @@ const style = {
   p: 4,
 };
 
-export default function WalletModal({
-  walletModalOpen,
-  handleWalletClose,
-  setWalletBalance,
-}) {
+export default function WalletModal({ walletModalOpen, handleWalletClose, setWalletBalance }) {
   const [errors, setErrors] = useState({});
   const [formData, setFormData] = useState({
-    title: "",
     price: "",
-    category: "",
-    date: "",
   });
 
   const handleChange = (event) => {
@@ -38,107 +30,89 @@ export default function WalletModal({
   };
 
   const validateForm = () => {
-    let temp = { ...errors };
-    // temp.title = formData.title ? "" : "This field is required.";
+    let temp = {};
     temp.price = formData.price ? "" : "This field is required.";
-    // temp.category = formData.category ? "" : "This field is required.";
-    // temp.date = formData.date ? "" : "This field is required.";
-    setErrors({
-      ...temp,
-    });
-    // Return true if no errors
+    setErrors({ ...temp });
     return Object.values(temp).every((x) => x === "");
   };
 
   const clearForm = () => {
-    setFormData({
-      title: "",
-      price: "",
-      category: "",
-      date: "",
-    });
+    setFormData({ price: "" });
     setErrors({});
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validateForm()) {
-      console.log("Form Submitted:", formData);
-
-      // Add expense to the list
-      //setExpenses((prevExpenses) => [...prevExpenses, formData]);
-
-      // Add income to the wallet balance
       setWalletBalance((prev) => prev + Number(formData.price));
       clearForm();
       handleWalletClose();
-    } else {
-      console.log("Form not submitted");
     }
   };
 
   return (
-    <div>
-      {/* <Button onClick={handleOpen}>Open modal</Button> */}
-      <Modal
-        open={walletModalOpen}
-        onClose={handleWalletClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+    <Modal
+      open={walletModalOpen}
+      onClose={handleWalletClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <Box
+        sx={style}
+        component="form"
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}       
       >
-        <Box sx={style} component="form" validate="true" autoComplete="off">
-          <Grid container spacing={2}>
-            <Grid item size={{ xs: 12, sm: 6, md: 6 }}>
-              <TextField
-                id="filled-basic"
-                label="Price"
-                variant="filled"
-                size="small"
-                name="price"
-                fullWidth
-                value={formData.price}
-                onChange={handleChange}
-                type="number"
-                error={!!errors.price}
-                helperText={errors.price}
-              />
-            </Grid>
-            <Grid item size={{ xs: 12, sm: 3, md: 3 }}>
-              <Button
-                fullWidth
-                sx={{
-                  backgroundColor: "#F4BB4A",
-                  color: "#FFFFFF",
-                  boxShadow: "0px 4px 4px 0px #00000040",
-                }}
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Add Balance
-              </Button>
-            </Grid>
-            <Grid item size={{ xs: 12, sm: 3, md: 3 }}>
-              <Button
-                fullWidth
-                sx={{
-                  backgroundColor:
-                    "linear-gradient(0deg, #D9D9D9, #D9D9D9), linear-gradient(0deg, #D9D9D9, #D9D9D9), linear-gradient(0deg, #E3E3E3, #E3E3E3)",
-                  color: "#000000",
-                  boxShadow: "0px 4px 4px 0px #00000040",
-                }}
-                onClick={handleWalletClose}
-              >
-                Cancel
-              </Button>
-            </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Income Amount"
+              placeholder="Income Amount"
+              variant="outlined"        
+              size="small"
+              name="price"
+              fullWidth
+              value={formData.price}
+              onChange={handleChange}
+              type="number"
+              error={!!errors.price}
+              helperText={errors.price}
+              inputProps={{ 'data-cy': 'income-input' }}
+            />
           </Grid>
-        </Box>
-      </Modal>
-    </div>
+
+          <Grid item xs={6}>
+            <Button
+              type="submit"
+              fullWidth
+              sx={{
+                backgroundColor: "#F4BB4A",
+                color: "#FFFFFF",
+                boxShadow: "0px 4px 4px 0px #00000040",
+              }}
+            >
+              Add Balance
+            </Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button
+              fullWidth
+              onClick={handleWalletClose}
+              sx={{
+                backgroundColor:
+                  "linear-gradient(0deg, #D9D9D9, #D9D9D9), linear-gradient(0deg, #D9D9D9, #D9D9D9), linear-gradient(0deg, #E3E3E3, #E3E3E3)",
+                color: "#000000",
+                boxShadow: "0px 4px 4px 0px #00000040",
+              }}
+            >
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Modal>
   );
 }
